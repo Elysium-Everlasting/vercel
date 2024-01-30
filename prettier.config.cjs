0e48305494bf41e28a7cb80e878ef2c3e62f0242
Aponia <brianmv@uci.edu>
2024-01-29 21:03:18 -0800

    feat: ✨ initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * @type {import('prettier').Options}
+ */
+const config = {
+  semi: false,
+  printWidth: 100,
+  singleQuote: true,
+  plugins: [],
+}
+
+module.exports = config
