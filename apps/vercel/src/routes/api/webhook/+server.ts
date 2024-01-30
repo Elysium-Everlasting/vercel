f0f9b5f72729469f1b92032e0e809fa345e7af29
Aponia <brianmv@uci.edu>
2024-01-30 00:04:25 -0800

    feat: ✨ webhook events

--- apps/vercel/src/routes/api/webhook/+server.ts
+++ apps/vercel/src/routes/api/webhook/+server.ts
@@ -5,37 +5,28 @@ import type { RequestHandler } from './$types'
 
 import { APP_ID, PRIVATE_KEY, WEBHOOK_SECRET } from '$env/static/private'
 
+const app = new App({
+  appId: APP_ID,
+  privateKey: PRIVATE_KEY,
+  webhooks: {
+    secret: WEBHOOK_SECRET,
+  },
+})
+
+app.webhooks.on('push', async ({ id, name, payload }) => {
+  console.log('PUSHED', { id, name, payload })
+})
+
 export const POST: RequestHandler = async (event) => {
-  console.log('HANDLING WEBHOOK')
-
-  const app = new App({
-    appId: APP_ID,
-    privateKey: PRIVATE_KEY,
-    webhooks: {
-      secret: WEBHOOK_SECRET,
-    },
-  })
-
-  const id = event.request.headers.get('x-github-delivery') ?? ''
-  const name = event.request.headers.get('x-github-event') as EmitterWebhookEventName
-  const signatureSHA256 = event.request.headers.get('x-hub-signature-256') ?? ''
+  const id = event.request.headers.get('X-Github-Delivery') ?? ''
+  const name = event.request.headers.get('X-Github-Event') as EmitterWebhookEventName
+  const signature = event.request.headers.get('x-hub-signature-256') ?? ''
   const payload = await event.request.text()
 
-  console.log({
-    id,
-    name,
-    signatureSHA256,
-    payload,
-  })
-
-  await app.webhooks.verifyAndReceive({
-    id: id,
-    name,
-    payload,
-    signature: signatureSHA256,
-  })
-
-  console.log('DONE HANDLING WEBHOOK')
-
-  return new Response('Hello from the webhook server!')
+  try {
+    await app.webhooks.verifyAndReceive({ id, name, payload, signature })
+    return new Response('OK')
+  } catch (error) {
+    return new Response('Error', { status: 500 })
+  }
 }
