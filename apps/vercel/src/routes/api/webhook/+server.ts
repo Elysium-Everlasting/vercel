a5d1ffbf7aca34b80655f799c9338e8370a08435
Aponia <brianmv@uci.edu>
2024-01-29 23:30:59 -0800

    feat: ✨ try to initialize octokit

--- apps/vercel/src/routes/api/webhook/+server.ts
+++ apps/vercel/src/routes/api/webhook/+server.ts
@@ -1,6 +1,27 @@
+import { App } from 'octokit'
+
 import type { RequestHandler } from './$types'
+
+import { APP_ID, PRIVATE_KEY, WEBHOOK_SECRET } from '$env/static/private'
 
 export const POST: RequestHandler = async () => {
   console.log('Received a webhook request!')
+
+  console.log({
+    APP_ID,
+    PRIVATE_KEY,
+    WEBHOOK_SECRET,
+  })
+
+  const app = new App({
+    appId: APP_ID,
+    privateKey: PRIVATE_KEY,
+    webhooks: {
+      secret: WEBHOOK_SECRET,
+    },
+  })
+
+  console.log('App created!', app)
+
   return new Response('Hello from the webhook server!')
 }
