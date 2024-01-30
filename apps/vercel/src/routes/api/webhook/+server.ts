fb24738ccab8d2e9387228757771a7187881021c
Aponia <brianmv@uci.edu>
2024-01-29 23:41:54 -0800

    feat: ✨ handle webhook event

--- apps/vercel/src/routes/api/webhook/+server.ts
+++ apps/vercel/src/routes/api/webhook/+server.ts
@@ -1,17 +1,12 @@
+import type { EmitterWebhookEventName } from '@octokit/webhooks'
 import { App } from 'octokit'
 
 import type { RequestHandler } from './$types'
 
 import { APP_ID, PRIVATE_KEY, WEBHOOK_SECRET } from '$env/static/private'
 
-export const POST: RequestHandler = async () => {
-  console.log('Received a webhook request!')
-
-  console.log({
-    APP_ID,
-    PRIVATE_KEY,
-    WEBHOOK_SECRET,
-  })
+export const POST: RequestHandler = async (event) => {
+  console.log('HANDLING WEBHOOK')
 
   const app = new App({
     appId: APP_ID,
@@ -21,7 +16,26 @@ export const POST: RequestHandler = async () => {
     },
   })
 
-  console.log('App created!', app)
+  const id = event.request.headers.get('x-github-delivery') ?? ''
+  const name = event.request.headers.get('x-github-event') as EmitterWebhookEventName
+  const signatureSHA256 = event.request.headers.get('x-hub-signature-256') ?? ''
+  const payload = await event.request.text()
+
+  console.log({
+    id,
+    name,
+    signatureSHA256,
+    payload,
+  })
+
+  await app.webhooks.verifyAndReceive({
+    id: id,
+    name,
+    payload,
+    signature: signatureSHA256,
+  })
+
+  console.log('DONE HANDLING WEBHOOK')
 
   return new Response('Hello from the webhook server!')
 }
