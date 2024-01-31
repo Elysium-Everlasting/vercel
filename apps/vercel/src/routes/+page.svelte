ad83ed6975e79b20144528edd37eb46a736bcd19
Aponia <brianmv@uci.edu>
2024-01-30 19:27:22 -0800

    feat: ✨ kind of looks like vercel

--- apps/vercel/src/routes/+page.svelte
+++ apps/vercel/src/routes/+page.svelte
@@ -4,15 +4,32 @@
   import * as Card from '$lib/components/ui/card'
   import type { PageData } from './$types'
 
+  interface StatusDisplay {
+    text: string
+    color: string
+  }
+
+  const statuses: Record<string, StatusDisplay> = {
+    ACTIVE: {
+      text: 'Ready',
+      color: 'bg-green-300',
+    },
+  }
+
+  function getStatusInfo(status?: string | null) {
+    if (!status) {
+      return { text: 'Unknown', color: 'bg-gray-100' }
+    }
+    return statuses[status] ?? { text: status, color: 'bg-gray-100' }
+  }
+
   export let data: PageData
 </script>
 
-<div>
-  <h1>Number of deployments: {data.repository?.deployments.edges?.length}</h1>
-</div>
-
-<div>
+<div class="max-w-7xl mx-auto">
   {#each data.repository?.deployments.edges ?? [] as deployment}
+    {@const status = getStatusInfo(deployment?.node?.state)}
+
     <Card.Root class="p-2">
       <Card.Content class="flex pb-0 justify-between">
         <div class="">
@@ -20,8 +37,12 @@
           <p>{deployment?.node?.latestEnvironment}</p>
         </div>
 
-        <div class="flex items-center">
-          <p>{deployment?.node?.state}</p>
+        <div class="flex items-center gap-2">
+          <div
+            class="w-3 h-3 rounded-full {status.color}"
+            style="background-color: ${status.color}"
+          ></div>
+          <p>{status.text}</p>
         </div>
 
         <div>
@@ -48,10 +69,12 @@
         </div>
 
         <div class="flex items-center">
-          <p>{deployment?.node?.commit?.committedDate} by {deployment?.node?.commit?.author?.name}</p>
+          <p>
+            {deployment?.node?.commit?.committedDate} by {deployment?.node?.commit?.author?.name}
+          </p>
           <div class="w-6 h-6 rounded-full overflow-hidden">
             <img
-              src="{deployment?.node?.commit?.author?.avatarUrl}"
+              src={deployment?.node?.commit?.author?.avatarUrl}
               alt="committer avatar"
               class="w-full h-full"
             />
